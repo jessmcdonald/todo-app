0e0c1f7d35a8b65198ad42ad514529b97950d2fa
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2019-11-05 18:10:28 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo" />
+
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id" v-bind:class="{'is-complete':todo.completed}">
+        <input type="checkbox" v-model="todo.completed" />
+
+        <span class="todo-label" @dblclick="editTodo(todo)" v-if="!todo.edit">{{ todo.label }}</span>
+
+        <input
+          v-else
+          class="todo-edit"
+          type="text"
+          v-model="todo.label"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.escape="doneEdit(todo)"
+        />
+
+        <button @click="removeTodo(todo)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          label: "Get cat food",
+          completed: false,
+          edit: false
+        },
+        {
+          id: 2,
+          label: "Bake cake",
+          completed: true,
+          edit: false
+        }
+      ],
+      currentTodo: "",
+      editedTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        edit: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    markComplete(todo) {
+      todo.completed = !todo.completed;
+    },
+    editTodo(todo) {
+      todo.edit = true;
+    },
+    doneEdit(todo) {
+      todo.edit = false;
+    }
   }
-}
+};
 </script>
 
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.is-complete {
+  text-decoration: line-through;
 }
 </style>
